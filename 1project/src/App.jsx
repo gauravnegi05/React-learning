852452257f01dee2f9af194004e29db2a50c7ae4
Gaurav Negi <gauravnegi6987@gmail.com>
2026-01-27 22:12:58 +0530

    hello

---
+++ 1project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Gaurav from "./gaurav";
+
+const App = () => {
+  return (
+    <>
+      <h1>hellloooo</h1>
+      <Gaurav />
+    </>
+  );
+};
+
+export default App;
---
+++ 1project/src/gaurav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Gaurav = () => {
+  return (
+    <div>
+      <h2>hellooo gaurav</h2>
+    </div>
+  )
+}
+
+export default Gaurav
