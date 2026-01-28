66b803c06119510d795f069c2a61e713e893297b
Gaurav Negi <gauravnegi6987@gmail.com>
2026-01-28 22:40:09 +0530

    projects

--- 1project/src/App.jsx
+++ 1project/src/App.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import Gaurav from "./gaurav";
 
 const App = () => {
+  const name = "gaurav";
   return (
     <>
-      <h1>hellloooo</h1>
+      <h1>hellloooo {name}</h1>
       <Gaurav />
     </>
   );
