865157c7b15b7a1be0dd8b9e4273e5fb5da80e43
Gaurav Negi <gauravnegi6987@gmail.com>
2026-01-28 22:39:45 +0530

    projects

--- 2project/src/App.jsx
+++ 2project/src/App.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import { useState } from "react";
 
 const App = () => {
-  return (
-    <div>
-      <h1>bhai kaise ho</h1>
-    </div>
-  )
-}
+  const [counter, setCounter] = useState(0);
 
-export default App
+  const addValue = () => {
+    setCounter(counter + 1);
+    console.log("clicked", counter + 1);
+  };
+
+  const removeValue = () => {
+    setCounter((prev) => (prev === 0 ? 0 : prev - 1));
+  };
+
+  return (
+    <>
+      <h1>bhai kaise ho</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addValue}>Add value</button>
+      <br />
+      <button onClick={removeValue}>Remove value</button>
+    </>
+  );
+};
+
+export default App;
