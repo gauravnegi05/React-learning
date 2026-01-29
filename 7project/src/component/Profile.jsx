641d9486d9d4f1d0be468293bfed2c1b17f4ca49
Gaurav Negi <gauravnegi6987@gmail.com>
2026-01-29 22:21:57 +0530

    7project

---
+++ 7project/src/component/Profile.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useContext } from 'react'
+import UserContext from '../context/UserContext'
+
+const Profile = () => {
+
+    const {user} = useContext(UserContext)
+
+    if (!user) return <div>please login</div>
+
+  return (
+    <div>
+      welcome {user.username}
+    </div>
+  )
+}
+
+export default Profile
