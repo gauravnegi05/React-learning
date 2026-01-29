bc0401173c16058392b3e5a09346bf6aabf5cf8e
Gaurav Negi <gauravnegi6987@gmail.com>
2026-01-29 21:43:23 +0530

    6project

---
+++ 6project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Home from './components/home/Home'
+import Footer from './components/footer/Footer'
+
+const App = () => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
